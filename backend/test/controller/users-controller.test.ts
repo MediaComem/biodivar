bd80594cbdd201bce78a0db5e3878a45e0d02fd9
Grégory Dozot <gregory.dozot@heig-vd.ch>
2022-01-21 12:27:53 +0100

    Finalize auth test

--- backend/test/controller/users-controller.test.ts
+++ backend/test/controller/users-controller.test.ts
@@ -2,35 +2,38 @@ import { Server } from "@hapi/hapi";
 
 import { init } from "../../src/server";
 import { setupConfig } from "../config/config";
-import { getUserById, getUserByName } from "../../src/controller/users-controller"
+import {
+  getUserById,
+  getUserByName,
+} from "../../src/controller/users-controller";
 import { setupUsers, dropUsers } from "../config/users";
 
-describe("Test server initialialization", () => {
-    let server: Server;
+describe("Test users controller", () => {
+  let server: Server;
 
-	beforeAll(async () => {
-        setupConfig();
-		server = await init();
-        await dropUsers(server.app.prisma);
-        await setupUsers(server.app.prisma);
-
-	})
-	afterAll( async() => {
-        await dropUsers(server.app.prisma);
-		await server.stop();
-	});
-
-    it("Find user by ID", async () => {
-        const existingUser = await getUserById(server.app.prisma, 1);
-        const notExistingUser = await getUserById(server.app.prisma, 3);
-        expect(existingUser?.username).toEqual("Rich");
-        expect(notExistingUser).toBeNull();
-	});
-
-    it("Find user by username", async () => {
-        const existingUser = await getUserByName(server.app.prisma, "Rach");
-        const notExistingUser = await getUserByName(server.app.prisma, "TEST");
-        expect(existingUser?.id).toEqual(2);
-        expect(notExistingUser).toBeNull();
-	});
+  beforeAll(async () => {
+    setupConfig();
+    server = await init();
+    await dropUsers(server.app.prisma);
+    await setupUsers(server.app.prisma);
   });
+
+  afterAll(async () => {
+    await dropUsers(server.app.prisma);
+    await server.stop();
+  });
+
+  it("Find user by ID", async () => {
+    const existingUser = await getUserById(server.app.prisma, 1);
+    const notExistingUser = await getUserById(server.app.prisma, 3);
+    expect(existingUser?.username).toEqual("Rich");
+    expect(notExistingUser).toBeNull();
+  });
+
+  it("Find user by username", async () => {
+    const existingUser = await getUserByName(server.app.prisma, "Rach");
+    const notExistingUser = await getUserByName(server.app.prisma, "TEST");
+    expect(existingUser?.id).toEqual(2);
+    expect(notExistingUser).toBeNull();
+  });
+});
---
+++ backend/test/router/auth-route.test.ts
@@ -0,0 +1,71 @@
+import { Server } from "@hapi/hapi";
+
+import { init } from "../../src/server";
+import { setupConfig } from "../config/config";
+import { setupUsers, dropUsers } from "../config/users";
+
+describe("Test server initialialization", () => {
+  let server: Server;
+
+  beforeAll(async () => {
+    setupConfig();
+    server = await init();
+    await dropUsers(server.app.prisma);
+    await setupUsers(server.app.prisma);
+  });
+
+  afterAll(async () => {
+    await dropUsers(server.app.prisma);
+    await server.stop();
+  });
+
+  it("Redirect to login page when not authenticate", async () => {
+    const res = await server.inject({
+      method: "GET",
+      url: "/",
+    });
+    expect(res.statusCode).toEqual(302);
+    expect(res.headers.location).toEqual("/login");
+  });
+
+  it("Login test failed", async () => {
+    const res = await server.inject({
+      method: "POST",
+      url: "/login",
+      payload: {
+        username: "Rich",
+        password: "badPassword",
+      },
+    });
+    expect(res.statusCode).toEqual(302);
+    expect(res.headers.location).toEqual("/login");
+  });
+
+  it("Login test success", async () => {
+    const res = await server.inject({
+      method: "POST",
+      url: "/login",
+      payload: {
+        username: "Rich",
+        password: "test",
+      },
+    });
+    expect(res.statusCode).toEqual(302);
+    expect(res.headers.location).toEqual("/");
+  });
+
+  it("Test access restricted page", async () => {
+    const res = await server.inject({
+      method: "GET",
+      url: "/",
+      auth: {
+        strategy: 'default',
+        credentials: {
+          id: 1,
+          password: "test"
+        }
+      }
+    });
+    expect(res.statusCode).toEqual(200);
+  });
+});
