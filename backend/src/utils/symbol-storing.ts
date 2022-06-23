5f8101bcb6ab7d366762cb8df852042303df048d
Grégory Dozot <gregory.dozot@heig-vd.ch>
2022-06-23 08:11:16 +0200

    implement file exists solution

--- backend/src/utils/symbol-storing.ts
+++ backend/src/utils/symbol-storing.ts
@@ -1,12 +1,26 @@
 import fs from 'fs';
+import path from 'path';
 import winston from 'winston';
 
-export const storeSymbol = async (userId: number, symbolName: String , symbol: Buffer, logger: winston.Logger) => {
+const fileExist = (origin: string, fullPath: string, next: number): string => {
+    if (fs.existsSync(fullPath)) {
+        const directories = path.dirname(fullPath);
+        const name = path.parse(origin).name;
+        const ext = path.parse(origin).ext;
+        return fileExist(origin, `${directories}/${name}_${next}${ext}`, next + 1);
+    }
+    return fullPath;
+}
+
+
+export const storeSymbol = async (userId: number, symbolName: string , symbol: Buffer, logger: winston.Logger) => {
     const symbolPath = `${process.env.SYMBOL_PATH}/${userId}`;
     try {
         fs.mkdirSync(symbolPath, {recursive: true});
-        fs.writeFileSync(`${symbolPath}/${symbolName}`, symbol);
-        return `${symbolPath}/${symbolName}`;
+        let fullPath = `${symbolPath}/${symbolName}`;
+        const finalPath = fileExist(symbolName, fullPath, 1);
+        fs.writeFileSync(finalPath, symbol);
+        return finalPath;
     }
     catch (error) {
         logger.error(error);
