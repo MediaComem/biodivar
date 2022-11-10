c1b74f848df55f07eae2f0a8c512a2622adbbb01
Grégory Dozot <gregory.dozot@heig-vd.ch>
2022-11-10 10:48:06 +0100

    Implement parameter slider

--- mobile/src/components/TheApp.vue
+++ mobile/src/components/TheApp.vue
@@ -40,7 +40,7 @@
   function closeMap() {
     window.dispatchEvent(new CustomEvent('close-ar-map', {}));
     menuOpen.value = false;
-    parameterOpen.value = false;
+    parameterOpen.value = false;f
   }
 
   watch(() => section.value, (newVal) => {
