4257a787a8e3b51ffe9bdf3d1c2e01cdbe166e6f
Grégory Dozot <gregory.dozot@heig-vd.ch>
2022-11-10 09:24:21 +0100

    Fix popup bug for event

--- mobile/src/components/TheMap/BaseEvent.vue
+++ mobile/src/components/TheMap/BaseEvent.vue
@@ -1,35 +1,37 @@
 <script setup>
-import { ref, onMounted, onUnmounted } from "@vue/runtime-core"; 
+import { ref, onMounted, onUnmounted, watch } from "@vue/runtime-core"; 
 
 import { mapStore } from '../../composables/map.js';
 
 const props = defineProps({
-    admin: Boolean,
-    event: Object,
-  });
+  admin: Boolean,
+  event: Object,
+});
 
-  const { map, mapAdmin } = mapStore();
+const { map, mapAdmin } = mapStore();
 
-  const currentMap = ref(null);
-  const circle = ref(null);
-  const popup = ref(null);
+const currentMap = ref(null);
+const circle = ref(null);
+const popup = ref(null);
 
 onMounted(() => {
-    props.admin ? currentMap.value = mapAdmin.value : currentMap.value = map.value;
-    circle.value = L.circle([
-        props.event.coordinate.lat,
-        props.event.coordinate.long], 
-        {
-          radius: 2,
-          weight: 1,
-          color: `#ff0000`,
-      }).addTo(currentMap.value);
-      popup.value = L.popup({closeButton: false, autoClose: false, autoPan: false})
-        .setLatLng([
-        props.event.coordinate.lat,
-        props.event.coordinate.long])
-        .setContent(`<p>${props.event.data}</p><p>${props.event.creation_date}</p>`)
-        .openOn(currentMap.value);
+  props.admin ? currentMap.value = mapAdmin.value : currentMap.value = map.value;
+  circle.value = L.circle([
+    props.event.coordinate.lat,
+    props.event.coordinate.long], 
+    {
+      radius: 2,
+      weight: 1,
+      color: `#ff0000`,
+    }).addTo(currentMap.value);
+  popup.value = L.popup({closeButton: false, autoClose: false, autoPan: false})
+    .setLatLng([
+      props.event.coordinate.lat,
+      props.event.coordinate.long])
+    .setContent(`<p>${props.event.data}</p><p>${props.event.creation_date}</p>`);
+  circle.value.on('click', () => {
+    popup.value.isOpen() ? currentMap.value.removeLayer(popup.value) : currentMap.value.addLayer(popup.value) ;
+  })
 })
 
 onUnmounted(() => {
