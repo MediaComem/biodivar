768ed5724000551d81b9ecd56030e82044d10a5c
Grégory Dozot <gregory.dozot@heig-vd.ch>
2022-11-10 11:37:42 +0100

    Improve menu in RA

--- mobile/src/components/TheHud/TheHudMenu.vue
+++ mobile/src/components/TheHud/TheHudMenu.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { ref } from "@vue/reactivity";
+  import { onMounted } from '@vue/runtime-core';
   import { useStore } from '../../composables/store.js';
   import { hubStore } from '../../composables/hubStore.js';
   import { clearHubTimeout, setHubTimeout } from './hub-utils.js';
@@ -33,6 +33,11 @@
     section.value = 'menu';
   }
 
+  onMounted(() => {
+    menuOpen.value = false;
+    parameterOpen.value = false;
+  })
+
 </script>
 
 <template>
@@ -49,11 +54,11 @@
   <div v-if="parameterOpen" class="menu-layout-parameter">
     <div class="menu-element" @click="closeParameter()"><p class="material-symbols-sharp menu-icon">west</p><p>paramètres</p></div>
     <div class="menu-element-parameter">
-      <p class="material-symbols-sharp" style="grid-area: 1 / 1 / 3 / 2; display: flex; align-items: center; justify-content: center">mode_of_travel</p>
-      <p style="grid-area: 1 / 2 / 2 / 3;">actualiser position</p>
-      <p style="grid-area: 2 / 2 / 3 / 3; font-size: 14px; display: flex; align-items: center">actualiser votre position après cette distance parcourue</p>
-      <p style="grid-area: 3 / 1 / 4 / 2; font-size: 20px; display: flex; align-items: center; justify-content: center">{{ minDistance }} m</p>
-      <input type="range" min="0.1" max="10" step="0.1" v-model="minDistance" style="grid-area: 3 / 2 / 4 / 3;"></div>  
+      <p class="material-symbols-sharp parameter-icon">mode_of_travel</p>
+      <p class="parameter-title">actualiser position</p>
+      <p class="paramter-description">actualiser votre position après cette distance parcourue</p>
+      <p class="parameter-label">{{ minDistance }} m</p>
+      <input type="range" min="0.1" max="10" step="0.1" v-model="minDistance" class="parameter-slider"></div>  
   </div>
 
 </template>
@@ -64,7 +69,7 @@
 p {
   margin: 0px;
   color: white;
-  font-size: 40px;
+  font-size: 32px;
 }
 
 .menu-layout {
@@ -96,7 +101,7 @@ p {
 .menu-element-parameter {
   height: 100px;
   display: grid;
-  grid-template-columns: 80px calc( 100% - 80px);
+  grid-template-columns: 72px calc( 100% - 72px);
   grid-template-rows: repeat(3, 33%);
   grid-column-gap: 0px;
   grid-row-gap: 0px;
@@ -105,6 +110,37 @@ p {
 .menu-icon {
   padding-left: 20px;
   padding-right: 20px;
+}
+
+.parameter-icon {
+  grid-area: 1 / 1 / 3 / 2; 
+  display: flex; 
+  align-items: center; 
+  justify-content: center
+}
+
+.parameter-title {
+  grid-area: 1 / 2 / 2 / 3;
+}
+
+.paramter-description {
+  grid-area: 2 / 2 / 3 / 3; 
+  font-size: 14px; 
+  display: flex; 
+  align-items: center; 
+  color: #BDBDBD
+}
+
+.parameter-label {
+  grid-area: 3 / 1 / 4 / 2; 
+  font-size: 20px; 
+  display: flex; 
+  align-items: center; 
+  justify-content: center
+}
+
+.parameter-slider {
+  grid-area: 3 / 2 / 4 / 3;
 }
 
 .icon-layout {
