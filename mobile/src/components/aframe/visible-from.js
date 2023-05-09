8734633d60157ea5810ccfc37050dcd6a6798850
Nicolas Chabloz <43348620+Chabloz@users.noreply.github.com>
2023-05-09 08:51:20 +0200

    Bugfixe: visibility change is now done a each throttled tick

--- mobile/src/components/aframe/visible-from.js
+++ mobile/src/components/aframe/visible-from.js
@@ -6,23 +6,15 @@ AFRAME.registerComponent('visible-from', {
   },
   init: function () {
     this.targetWorldPos = new THREE.Vector3();
-    this.myWorldPos = new THREE.Vector3();
-    this.isVisible = false;
-    this.el.setAttribute('visible', this.isVisible);
+    this.myWorldPos = new THREE.Vector3();    
+    this.el.setAttribute('visible', false);
     this.tick = AFRAME.utils.throttleTick(this.checkDist, this.data.throttle, this);
   },
   checkDist: function () {
     if (!this.data.target) return;
     this.data.target.object3D.getWorldPosition(this.targetWorldPos);
     this.el.object3D.getWorldPosition(this.myWorldPos);
-
-    const distance = this.myWorldPos.distanceTo(this.targetWorldPos);
-    if (distance <= this.data.distance && !this.isVisible) {
-      this.isVisible = true;
-      this.el.setAttribute('visible', this.isVisible);
-    } else if (distance > this.data.distance && this.isVisible) {
-      this.isVisible = false;
-      this.el.setAttribute('visible', this.isVisible);
-    }
+    const distance = this.myWorldPos.distanceTo(this.targetWorldPos);    
+    this.el.setAttribute('visible', distance <= this.data.distance);        
   }
 });
